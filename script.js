de483302a069b00f9613d3d4a8cb61328971c34b
Gagan Gadge <63653927+gagan66@users.noreply.github.com>
2022-01-22 12:26:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// let button = document.querySelector("#download");
+// button.addEventListener("click", () => {
+//     const a = document.querySelector("a");
+//     button.style.paddingRight = "10px";
+//     a.style.visibility = "visible";
+//     setTimeout(() => {
+//         a.style.visibility = "hidden";
+//         button.style.transition = "1s ease-in-out";
+//         button.style.paddingRight = "0px"
+
+//     }, 2000)
+// })
